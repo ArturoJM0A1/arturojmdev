91a561badc126596eb70cd7fc5e28f20297328bb
Arturo Juárez Monroy <juarezmonroyarturo574@gmail.com>
2026-04-03 23:26:30 -0600

    add img birrete.webp

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,11 +1,20 @@
-export default function EducationSection() {
+﻿export default function EducationSection() {
   return (
     <section className="education">
       <h3>Educacion</h3>
       <div className="education-item">
-        <h4>Ingenieria en Software</h4>
-        <p>Universidad Autonoma del Estado de Hidalgo (2020 - 2024)</p>
+        <div className="education-copy">
+          <h4>Ingenieria en Software</h4>
+          <p>Universidad Autonoma del Estado de Hidalgo (2020 - 2024)</p>
+        </div>
+        <img
+          className="education-icon"
+          src="/biirrete.webp"
+          alt=""
+          aria-hidden="true"
+        />
       </div>
     </section>
   );
 }
+
